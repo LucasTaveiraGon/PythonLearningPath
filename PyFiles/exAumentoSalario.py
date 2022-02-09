53ee99f5ff4c5dc1855198123b4dfb6c1bb121ba
Lucas Taveira <Lucastaveirag@gmail.com>
2022-02-09 17:21:44 -0300

    new exercises

---
+++ PyFiles/exAumentoSalario.py
@@ -0,0 +1,2 @@
+salario = float(input("Digita seu salario aí: "))
+print(f'seu novo salario é de {salario + (salario*15/100)}')
---
+++ PyFiles/exPromocao.py
@@ -0,0 +1,3 @@
+preco = float(input('Insira o preco de um produto para ver seu desconto: '))
+desc = preco - (preco*5/100)
+print(f'O novo preço com desconto de 5% é {desc} ')
